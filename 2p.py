393c63b814d910b350c79da5299b075500410d4f
JoeUgly <theejoe@aol.com>
2018-11-26 21:33:52 -0500

    phase 1

--- 2p.py
+++ 2p.py
@@ -3,35 +3,45 @@
 # Description: Search the provided webpages for keyword and attempt relavent crawling.
 
 # To do:
-# Phase 1: basic function
-# keyword search +
-# crawl function +
-# proper html parsing 
-# spoof user agent
-# 
-# Phase 2: basic optimization
-# prevent dups and checked pages
-# blacklist
-# error log
-# Phase 3: advanced features
+# Phase 1: Basic function
+# proper html parsing - 404 and 403 error 7 +
+# spoof user agent +
+# Phase 2: Basic optimization
+# prevent dups and checked pages +
+# error log +
+# open in browser
+# limit 10 results per domain +
+# multiple keywords
+# Phase 3: Advanced features
 # parellelization
 # user-defined levels of crawling
-# Phase 4: distribution
+# Phase 4: Distribution
 # enable cross-platform
 # Phase 5: GUI
 
+#error 3,4,6 - 9
+
+# Start timer
+from datetime import datetime
+startTime = datetime.now()
+
 import urllib.request, urllib.parse, urllib.error, os
 
-keyword = 'librarian'
+keyword = 'plant operator'
 user_agent = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:63.0) Gecko/20100101 Firefox/63.0'
-
+progresscount = 0
+pagecount = 0
 
 
 allcivurls = []
 errorurls = {}
-abspath = 'aaa'
-blacklisth = open(r'''C:\Users\jschiffler\Desktop\Text_n_Stuff\current\blacklist.txt''')
-blacklist = blacklisth.read()
+abspath = None
+blacklisthand = open(r'''/home/joepers/code/current/civ_crawl/blacklist''')
+blacklist = blacklisthand.read()
+
+## review and print this somewhere
+domainlimit = {}
+domainlimitset = set()
 
 keywordurlset = set()
 urllistprefilter = []
@@ -41,14 +51,17 @@ urllistgood = set()
 alltags = set()
 checkedurls = set()
 
+
+
 # Clear errorlog
-f = open("errorlog.txt", "w")
+f = open("/home/joepers/code/current/civ_crawl/errorlog", "w")
 f.write('')
 
 # Get portal URLs from file
-civfile = open(r'''C:\Users\jschiffler\Desktop\Text_n_Stuff\current\2civil.txt''')
+civfile = open(r'''/home/joepers/code/current/civ_crawl/civil_ny''')
 for civline in civfile:
     allcivurls.append(civline)
+numcivurls = len(allcivurls)
 
 for eachcivurl in allcivurls:
     alltags.clear()
@@ -56,16 +69,19 @@ for eachcivurl in allcivurls:
     urllist2.clear()
     urllistgood.clear()
     urllistprefilter.clear()
-    print('\n\n\n\n ====================== Start =====================\n', eachcivurl)
+    print('\n\n\n\n ============================= Start ============================\n', eachcivurl)
+    progresscount += 1
+    pagecount += 1
     domain = eachcivurl.rsplit('/', 1)[0]
-    print('domain = ', domain)
-
+    domainlimit[domain] = 0
+    print('Progress =', progresscount, 'of', numcivurls, '\ndomain = ', domain)
 
     # Get html from url
     try:
         #html = urllib.request.urlopen(eachcivurl)
-        ##html = response.read()
+        #html = response.read()
         
+        # Spoof user agent
         request = urllib.request.Request(eachcivurl,headers={'User-Agent': user_agent})
         html = urllib.request.urlopen(request)
         
@@ -83,36 +99,37 @@ for eachcivurl in allcivurls:
             #dechtml = html.read().decode(errors='ignore')
             dechtml = html.read().decode()
         except Exception as errex:
-            print('error 2: decode at ', eachcivurl)
-            errorurls[eachcivurl] = 'error 2', errex
+            print('error 2: decode at ', eachcivurl, str(errex)[:99])
+            errorurls[eachcivurl] = 'error 2', str(errex)[:99]
             continue
 
     else:
         try:
             dechtml = html.read().decode(charset_encoding)
         except Exception as errex:
-            print('error 2: decode at ', eachcivurl)
-            errorurls[eachcivurl] = 'error 2', errex
+            print('error 2: decode at ', eachcivurl, str(errex)[:99])
+            errorurls[eachcivurl] = 'error 2', str(errex)[:99]
             continue
 
-    html1 = dechtml.lower()
+    dechtml1 = dechtml.lower()
 
     # Search for keyword on page
-    keycheck = html1.find(keyword)
+    #keycheck = None
+    #keycheck = html1.find(keyword)
 
     # Add to the keywordurl set
-    if keycheck != -1:
-        print('\n~~~ Found keyword ~~~\n')
+    if keyword in dechtml1:
+        print('\n~~~~~~ Keyword match ~~~~~~\n')
         keywordurlset.add(eachcivurl)
     else:
-        print('\n~~~ Not today ~~~\n')
+        print('\n~~~ No match ~~~\n')
 
     # Add to checked pages set
     checkedurls.add(eachcivurl)
     
-
+    ## href= as delimiter?
     # Seperate html into lines using <a delimiter
-    htmllines = html1.split('<a ')
+    htmllines = dechtml1.split('<a ')
     #htmllines.sort
 
     # End lines using </a> delimiter and add to alltags set
@@ -135,34 +152,32 @@ for eachcivurl in allcivurls:
 
     # Determine if the tag contains a jobword
     for tag in alltags:
-        if any(xxx in tag for xxx in jobwords):
 
-            # Append only the url to the list
-            if tag.count('href=') > 0:
-                try:
-                    urlline0 = tag.split('href=')[1]
 
-                    # Determine if double or single quote comes first in tag
-                    dqloc = urlline0.find('"')
-                    sqloc = urlline0.find("'")
+        # Append only the url to the list
+        if tag.count('href') > 0:
+            urlline0 = tag.split('href')[1]
+            #print('ul0 = ', urlline0)
 
-                    if dqloc < sqloc:
-                        if dqloc > -1:
-                            quovar = '"'
-                        else: quovar = "'"
-                    elif dqloc > sqloc:
-                        if sqloc > -1:
-                            quovar = "'"
-                        else: quovar = '"'
-                    else:
-                        print('error 3: tag quote at ', tag)
-                        errorurls[tag] = 'error 3: tag quote'
+            if any(xxx in urlline0 for xxx in jobwords):
 
-                except Exception as errex:
-                    print('error 4: url split at ', tag)
-                    errorurls[tag] = 'error 4', errex
+                # Determine if double or single quote comes first in tag
+                dqloc = urlline0.find('"')
+                sqloc = urlline0.find("'")
 
-                    
+                if dqloc < sqloc:
+                    if dqloc > -1:
+                        quovar = '"'
+                    else: quovar = "'"
+                elif dqloc > sqloc:
+                    if sqloc > -1:
+                        quovar = "'"
+                    else: quovar = '"'
+                else:
+                    print('error 3: tag quote at ', tag)
+                    errorurls[tag] = 'error 3: tag quote'
+
+                
                 urlline = urlline0.split(quovar)[1]
 
                 # Convert any rel paths to abs
@@ -179,23 +194,46 @@ for eachcivurl in allcivurls:
 
                         # Exclude if the abspath is a checked page
                         if not abspath in checkedurls:
-                            urllistgood.add(abspath)                
+                            urllistgood.add(abspath)    
+            
 
-            ## error 6 should not be an error
-            else:
-                print('error 5: no "href=" at ', tag)
-                errorurls[tag] = 'error 6: no href='
+        ## error 5 should not be an error
+        else:
+            print('error 5: no "href=" at ', tag)
+            #errorurls[tag] = 'error 5: no "href="'
+            #continue
+
+    # Show excluded urls
+    excludedbybw = list(set(urllistprefilter) - set(urllist1))
+    excludedbybl = list(set(urllist1) - set(urllist2))
+    excludedbydups = list(set(urllist2) - set(urllistgood))
+
+    print('excluded by bunkwords = ', excludedbybw, '\nexcluded by blacklist = ', excludedbybl, '\nexcluded by dups = ', excludedbydups)
+    #print('ul2 = ', urllist2, 'ulg = ', urllistgood)
+
+
+
     try:
-        print('tag = ', tag, '\nurlline = ', urlline, '\nabsp = ', abspath, '\nul1= ', len(urllist1), '\nul2 = ', len(urllist2), '\nulg = ', len(urllistgood))
+        print('\nul1nbw = ', len(urllist1), '\nul2nbl = ', len(urllist2), '\nulgndups = ', len(urllistgood))
     except:
         print('error 6:')
 
+
+    # Begin crawl
+    print('\n-------------------- Begin crawl ----------------------\n')
     for workingurl in urllistgood:
-        print('------------- Begin crawl -------------\n', workingurl)
+        pagecount += 1
+        print('\n', workingurl)
 
         # Get html from url
         try:
-            workinghtml = urllib.request.urlopen(workingurl)
+
+            # Spoof user agent
+            workingrequest = urllib.request.Request(eachcivurl,headers={'User-Agent': user_agent})
+            workinghtml = urllib.request.urlopen(workingrequest)
+
+
+            #workinghtml = urllib.request.urlopen(workingurl)
         except Exception as errex:
             print('error 7: url request at', workingurl, errex)
             errorurls[workingurl] = 'error 7', errex
@@ -207,59 +245,55 @@ for eachcivurl in allcivurls:
             try:
                 decworkinghtml = html.read().decode(errors='ignore')
             except Exception as errex:
-                print('error 8: decode at ', workingurl)
-                errorurls[workingurl] = 'error 8', errex
+                print('error 8: decode at ', workingurl, str(errex)[:99])
+                errorurls[workingurl] = 'error 8', str(errex)[:99]
                 continue
         else:
             try:
                 decworkinghtml = workinghtml.read().decode(charset_encoding)
             except Exception as errex:
-                print('error 8: decode at ', workingurl)
-                errorurls[workingurl] = 'error 8', errex
+                print('error 9: decode at ', workingurl, str(errex)[:99])
+                errorurls[workingurl] = 'error 9', str(errex)[:99]
                 continue
 
         decworkinghtml1 = decworkinghtml.lower()
 
         # Search for keyword on page
-        keycheck = decworkinghtml1.find(keyword)
+        #keycheck = None
+        #keycheck = decworkinghtml1.find(keyword)
+        #print('8888888888', decworkinghtml1)
 
         # Add to the keywordurl set
-        if keycheck != -1:
-            print('\n~~~ Found keyword ~~~\n')
-            keywordurlset.add(workingurl)
+        if keyword in decworkinghtml1:
+            print('\n~~~~~~ Keyword match ~~~~~~\n')
+            if domainlimit[domain] < 12:
+                keywordurlset.add(workingurl)
+                domainlimit[domain] += 1
+            else:
+                print('Match omitted. Domain limit reached.')
+                domainlimitset.add(workingurl)
         else:
-            print('\n~~~ Not today ~~~\n')
+            print('~~~ No match ~~~\n')
 
         # Add to checked pages set
         checkedurls.add(workingurl)
 
 
+# Results
+print('\n\n\n ################################ ', len(keywordurlset), ' matches found at: ###################\n')
+for i in sorted(list(keywordurlset)):
+    print(i + '\n')
 
+# Display and write errorlog
+writeerrors = open("/home/joepers/code/current/civ_crawl/errorlog", "a")
+print('\n\n', len(errorurls), ' errors found at: \n',)
+for k, v in errorurls.items():
+    print(v, '::', k, '\n')
+    vk = str((v, '::', k))
+    writeerrors.write(vk + '\n\n')
 
-
-
-    print('\n\n~~~~~~~~~~~~ urllistprefilter length =', len(urllistprefilter), ' ~~~~~~~~~~~~\n', sorted(urllistprefilter))
-
-    print('\n\n~~~~~~~~~~~~ urllistgood length =', len(urllistgood), ' ~~~~~~~~~~~~\n', sorted(urllistgood))
-
-
-
-    print('\n\n ################## ', len(keywordurlset), ' matches found at: \n', keywordurlset)
-
-
-
-
-    # Display and write errorlog
-    writeerrors = open("errorlog.txt", "a")
-    print('\n\n', len(errorurls), ' errors found at: \n',)
-    for k, v in errorurls.items():
-        print(v, '::', k, '\n')
-        vk = str((v, '::', k))
-        writeerrors.write(vk + '\n')
-
-
-
-
+# Stop timer
+print('churls = ', len(checkedurls), 'Number of pages checked = ', pagecount, '\nDuration = ', datetime.now() - startTime)
 
 
 
