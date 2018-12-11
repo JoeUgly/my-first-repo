de8afb5613f773f9bb6a0db881708462714d57d2
Joseph Schiffler <jschiffler@BataviaNewYork.com>
2018-12-11 13:53:41 -0500

    3t working

--- 2test.py
+++ 2test.py
@@ -1,6 +1,95 @@
 
 
+import os, traceback, time, os
 
-a = 0 / 2
+from multiprocessing import Process, Queue, Lock
+import queue
 
-print(a)
+
+num_threads = 26
+civfile = open(r'''C:\Users\jschiffler\Desktop\Text_n_Stuff\current\civil_ny.txt''')
+
+
+allcivurls = Queue()
+for civline in civfile:
+    allcivurls.put(civline)
+
+qlength = allcivurls.qsize()
+tasks_that_are_done = Queue()
+
+
+
+
+def crawler(allcivurls, tasks_that_are_done, lock):
+    while True:
+        lock.acquire()
+        try:
+            eachcivurl = allcivurls.get_nowait()
+            print('here1', eachcivurl, tasks_that_are_done.qsize())
+            
+        except queue.Empty:
+            print('here2')
+            lock.release()
+            break
+
+        else:
+            print('here3')
+
+            print('here4\n\n')
+            tasks_that_are_done.put('YYYYYYYYYYYYYYYYYYY' + str(os.getpid()))
+            lock.release()
+
+
+
+
+
+if __name__ == '__main__':
+    lock = Lock()
+    print('xxxxxxxxxxxxx', tasks_that_are_done.qsize(), qlength)
+
+    print('join_count')
+
+
+
+    for ii in range(num_threads):
+        worker = Process(target=crawler, args=(allcivurls, tasks_that_are_done, lock))
+        worker.start()
+
+
+
+        
+    while True:
+        if tasks_that_are_done.qsize() >= qlength:
+            print('done', tasks_that_are_done.qsize(), qlength)
+            break
+        else:
+            print('waiting', tasks_that_are_done.qsize(), qlength)
+            time.sleep(1)
+
+
+    print('YYAAAAYYY')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- mp.py
+++ mp.py
@@ -1,3 +1,146 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+import os, traceback, time, os
+
+from multiprocessing import Process, Queue, Lock
+import queue
+
+
+num_threads = 26
+civfile = open(r'''C:\Users\jschiffler\Desktop\Text_n_Stuff\current\civil_ny.txt''')
+
+
+allcivurls = Queue()
+for civline in civfile:
+    allcivurls.put(civline)
+
+qlength = allcivurls.qsize()
+tasks_that_are_done = Queue()
+
+
+
+
+def crawler(allcivurls, tasks_that_are_done, lock):
+    while True:
+        lock.acquire()
+        try:
+            eachcivurl = allcivurls.get_nowait()
+            print('here1', eachcivurl, tasks_that_are_done.qsize())
+            
+        except queue.Empty:
+            print('here2')
+            lock.release()
+            break
+
+        else:
+            print('here3')
+
+            print('here4\n\n')
+            tasks_that_are_done.put('YYYYYYYYYYYYYYYYYYY' + str(os.getpid()))
+            lock.release()
+
+
+
+
+
+if __name__ == '__main__':
+    lock = Lock()
+    print('xxxxxxxxxxxxx', tasks_that_are_done.qsize(), qlength)
+
+    print('join_count')
+
+
+
+    for ii in range(num_threads):
+        worker = Process(target=crawler, args=(allcivurls, tasks_that_are_done, lock))
+        worker.start()
+
+
+
+        
+    while True:
+        if tasks_that_are_done.qsize() >= qlength:
+            print('done', tasks_that_are_done.qsize(), qlength)
+            break
+        else:
+            print('waiting', tasks_that_are_done.qsize(), qlength)
+            time.sleep(1)
+
+
+    print('YYAAAAYYY')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #!/usr/bin/python3.7
 
 # Description: Search the provided webpages for keyword and attempt relavent crawling.
@@ -117,9 +260,12 @@ def crawler(allcivurls, tasks_that_are_done):
         try:
             # Begin fetching    
             eachcivurl = allcivurls.get_nowait()
+            print('here1')
 
         except queue.Empty:
+            print('here2')            
             break
+        
 
         else:
             print('eachcivurl = ', eachcivurl)
@@ -362,19 +508,19 @@ def crawler(allcivurls, tasks_that_are_done):
             print('------------ End of function ------------  PID =', os.getpid())
 
         finally:
+            print('here3\n\n')            
             tasks_that_are_done.put('YYYYYYYYYYYYYYYYYYY' + str(os.getpid()))
 
-    return True
 
 
 ####   End of function   ####
 
 
-print('xxxxxxxxxxxxx', tasks_that_are_done.qsize(), qlength)
 
 
 if __name__ == '__main__':
     join_list = 0
+    print('xxxxxxxxxxxxx', tasks_that_are_done.qsize(), qlength)
 
 
 
@@ -389,7 +535,7 @@ if __name__ == '__main__':
     
     for ooo in range(join_list):
         print('\n\nWaiting for threads ...', wait_count, join_list)
-        worker.join()
+        #worker.join()
         wait_count += 1
 
 while True:
